28c68babef0dc12f7949c4a02d93e9256d21d88d
Nastya Luna <n.vzdor@gmail.com>
2019-10-10 12:55:38 +0300

    Modules: Auth, Dashboard, App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import App from './App';
+import App from './App/App';
 import './index.css';
 
 ReactDOM.render(
