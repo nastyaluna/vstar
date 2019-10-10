0ba543e50e4dda7e281b32e9aeac5a982dabed4e
Nastya Luna <n.vzdor@gmail.com>
2019-10-10 13:15:17 +0300

    Router added. Module: Home.

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from '../Home/Home';
+import SignIn from '../Auth/SignIn';
+import SignUp from '../Auth/SignUp';
+import Dashboard from '../Dashboard/Dashboard';
 const muiTheme = createMuiTheme();
 
 const App = () => {
   return (
       <MuiThemeProvider theme={muiTheme}>
         <CssBaseline/>
-        <h1>Vstar</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={SignIn} />
+            <Route exact path="/register" component={SignUp} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </Router>
       </MuiThemeProvider>
   );
 };
