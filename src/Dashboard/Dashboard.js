bbb6e31f35fb95c0797bb77b44b1917b646b1eca
Nastya Luna <n.vzdor@gmail.com>
2019-10-10 17:32:49 +0300

    Dashboard UI and logic

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Lottie from 'react-lottie';
+import {Link} from 'react-router-dom';
 import makeAnimation from '../utils/makeAnimation';
 import animationData from './astronaut';
 import {makeStyles} from '@material-ui/core';
+import Button from "@material-ui/core/Button";
+import {logout} from '../Auth/actions';
+import {connect} from "react-redux";
 
 const useStyles = makeStyles(theme => ({
   dashboard: {
-    display: 'grid'
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }));
 
-const Dashboard = () => {
+const Dashboard = ({logout}) => {
   const cls = useStyles();
   const animationOptions = makeAnimation(animationData);
+
+  const onLogout = () => logout();
 
   return (
       <div className={cls.dashboard}>
@@ -21,8 +30,18 @@ const Dashboard = () => {
             height={400}
             options={animationOptions}
         />
+        <Button
+            to="/"
+            type="submit"
+            color="primary"
+            component={Link}
+            onClick={onLogout}
+            variant="contained"
+        >
+          Come back to Earth
+        </Button>
       </div>
   );
 };
 
-export default Dashboard;
+export default connect(null, {logout})(Dashboard);
